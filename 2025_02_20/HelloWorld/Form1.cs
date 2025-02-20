72e91fb8ea4a8eacfa94ebea9f820f98a90b94bb
yuxuan72 <a113221072@mail.shu.edu.tw>
2025-02-20 11:49:19 +0800

    2025-02-20

---
+++ 2025_02_20/HelloWorld/Form1.cs
@@ -0,0 +1,15 @@
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "­n¨Ä¨Ä·í§L!";
+        }
+    }
+}
