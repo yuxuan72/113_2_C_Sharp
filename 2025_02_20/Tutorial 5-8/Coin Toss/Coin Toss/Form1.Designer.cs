9cd95a5fbfb42dbfee93c048356a7a991b248754
yuxuan72 <a113221072@mail.shu.edu.tw>
2025-02-20 12:48:21 +0800

    2025-02-20

---
+++ 2025_02_20/Tutorial 5-8/Coin Toss/Coin Toss/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Coin_Toss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tailsPictureBox = new System.Windows.Forms.PictureBox();
+            this.headsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tossButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tailsPictureBox
+            // 
+            this.tailsPictureBox.Image = global::Coin_Toss.Properties.Resources.Tails1;
+            this.tailsPictureBox.Location = new System.Drawing.Point(33, 12);
+            this.tailsPictureBox.Name = "tailsPictureBox";
+            this.tailsPictureBox.Size = new System.Drawing.Size(170, 170);
+            this.tailsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tailsPictureBox.TabIndex = 1;
+            this.tailsPictureBox.TabStop = false;
+            this.tailsPictureBox.Visible = false;
+            // 
+            // headsPictureBox
+            // 
+            this.headsPictureBox.Image = global::Coin_Toss.Properties.Resources.Heads1;
+            this.headsPictureBox.Location = new System.Drawing.Point(33, 12);
+            this.headsPictureBox.Name = "headsPictureBox";
+            this.headsPictureBox.Size = new System.Drawing.Size(170, 170);
+            this.headsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.headsPictureBox.TabIndex = 0;
+            this.headsPictureBox.TabStop = false;
+            // 
+            // tossButton
+            // 
+            this.tossButton.Location = new System.Drawing.Point(40, 194);
+            this.tossButton.Name = "tossButton";
+            this.tossButton.Size = new System.Drawing.Size(75, 23);
+            this.tossButton.TabIndex = 2;
+            this.tossButton.Text = "Toss";
+            this.tossButton.UseVisualStyleBackColor = true;
+            this.tossButton.Click += new System.EventHandler(this.tossButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(121, 194);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 229);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tossButton);
+            this.Controls.Add(this.headsPictureBox);
+            this.Controls.Add(this.tailsPictureBox);
+            this.Name = "Form1";
+            this.Text = "Coin Toss";
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox headsPictureBox;
+        private System.Windows.Forms.PictureBox tailsPictureBox;
+        private System.Windows.Forms.Button tossButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
