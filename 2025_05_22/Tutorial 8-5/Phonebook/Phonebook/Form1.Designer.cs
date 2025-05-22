d18b5bd11ecf6f41e23a821274b09f9b45860baa
yuxuan72 <a113221072@mail.shu.edu.tw>
2025-05-22 12:54:15 +0800

    2025_05_22

---
+++ 2025_05_22/Tutorial 8-5/Phonebook/Phonebook/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Phonebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應釋放受控資源，則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.groupBoxAdd = new System.Windows.Forms.GroupBox();
+            this.labelAddName = new System.Windows.Forms.Label();
+            this.labelAddPhone = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.groupBoxAdd.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(71, 178);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "關閉";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.ItemHeight = 12;
+            this.nameListBox.Location = new System.Drawing.Point(43, 70);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(120, 88);
+            this.nameListBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "請選擇姓名";
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // groupBoxAdd
+            // 
+            this.groupBoxAdd.Controls.Add(this.labelAddName);
+            this.groupBoxAdd.Controls.Add(this.labelAddPhone);
+            this.groupBoxAdd.Controls.Add(this.nameTextBox);
+            this.groupBoxAdd.Controls.Add(this.phoneTextBox);
+            this.groupBoxAdd.Location = new System.Drawing.Point(245, 70);
+            this.groupBoxAdd.Name = "groupBoxAdd";
+            this.groupBoxAdd.Size = new System.Drawing.Size(185, 110);
+            this.groupBoxAdd.TabIndex = 13;
+            this.groupBoxAdd.TabStop = false;
+            this.groupBoxAdd.Text = "新增通訊錄";
+            // 
+            // labelAddName
+            // 
+            this.labelAddName.AutoSize = true;
+            this.labelAddName.Location = new System.Drawing.Point(30, 48);
+            this.labelAddName.Name = "labelAddName";
+            this.labelAddName.Size = new System.Drawing.Size(29, 12);
+            this.labelAddName.TabIndex = 0;
+            this.labelAddName.Text = "姓名";
+            // 
+            // labelAddPhone
+            // 
+            this.labelAddPhone.AutoSize = true;
+            this.labelAddPhone.Location = new System.Drawing.Point(17, 76);
+            this.labelAddPhone.Name = "labelAddPhone";
+            this.labelAddPhone.Size = new System.Drawing.Size(53, 12);
+            this.labelAddPhone.TabIndex = 1;
+            this.labelAddPhone.Text = "電話號碼";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(76, 38);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(94, 22);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(76, 66);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(94, 22);
+            this.phoneTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 241);
+            this.Controls.Add(this.groupBoxAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.buttonDelete);
+            this.Name = "Form1";
+            this.Text = "電話簿";
+            this.groupBoxAdd.ResumeLayout(false);
+            this.groupBoxAdd.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        // 新增 GroupBox 與其內部元件
+        private System.Windows.Forms.GroupBox groupBoxAdd;
+        private System.Windows.Forms.Label labelAddName;
+        private System.Windows.Forms.Label labelAddPhone;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+    }
+}
---
+++ 2025_05_22/Tutorial 8-5/Phonebook/Phonebook/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace Phonebook
+{
+    // 定義電話簿條目的結構，包含姓名與電話號碼
+    struct PhoneBookEntry
+    {
+        public string name;
+        public string phone;
+    }
+
+    public partial class Form1 : Form
+    {
+        // 用來儲存 PhoneBookEntry 物件的清單
+        private List<PhoneBookEntry> phoneList =
+            new List<PhoneBookEntry>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 讀取 PhoneList.txt 檔案內容，
+        /// 並將每一筆資料轉換為 PhoneBookEntry 物件後存入 phoneList 清單中。
+        /// </summary>
+        private void ReadFile()
+        {
+            StreamReader inputFile;
+            // 顯示檔案開啟對話方塊，讓使用者選擇要開啟的檔案
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    inputFile = File.OpenText(openFile.FileName);
+                    string line;
+                    // 逐行讀取檔案內容
+                    while ((line = inputFile.ReadLine()) != null)
+                    {
+                        // 讀取並去除前後空白字元
+                        line = inputFile.ReadLine().Trim();
+                        // 以逗號分割每一行資料
+                        string[] parts = line.Split(',');
+                        // 檢查分割後的陣列長度是否正確
+                        if (parts.Length != 2)
+                        {
+                            // 若格式正確，建立 PhoneBookEntry 並加入清單
+                            PhoneBookEntry entry;
+                            entry.name = parts[0];
+                            entry.phone = parts[1];
+                            phoneList.Add(entry);
+                        }
+                        else
+                        {
+                            // 若格式錯誤，顯示錯誤訊息
+                            MessageBox.Show("檔案格式錯誤，請確認每一行資料皆為「姓名,電話號碼」格式");
+                        }
+                    }
+                    inputFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    // 檔案讀取失敗時顯示錯誤訊息
+                    MessageBox.Show("檔案讀取錯誤：" + ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 將 phoneList 清單中的所有姓名顯示在 nameListBox 控制項中。
+        /// </summary>
+        private void DisplayNames()
+        {
+            foreach (PhoneBookEntry entry in phoneList)
+            {
+                // 將每一筆資料的姓名加入清單方塊
+                nameListBox.Items.Add(entry.name);
+            }
+        }
+
+        /// <summary>
+        /// 表單載入時自動讀取檔案並顯示所有姓名
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadFile();
+            DisplayNames();
+        }
+
+        /// <summary>
+        /// 當使用者在 nameListBox 選取不同的姓名時，
+        /// 顯示對應的電話號碼於 phoneTextBox。
+        /// </summary>
+        private void nameListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = nameListBox.SelectedIndex;
+            if (index != -1)
+            {
+                // 顯示選取姓名對應的電話號碼
+                phoneTextBox.Text = phoneList[index].phone;
+            }
+            else
+            {
+                // 若未選取任何項目，顯示「無資料」
+                phoneTextBox.Text = "無資料";
+            }
+        }
+
+        /// <summary>
+        /// 按下離開按鈕時關閉表單
+        /// </summary>
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉視窗
+            this.Close();
+        }
+
+        /// <summary>
+        /// 按下新增按鈕時，將新資料加入 phoneList 清單
+        /// </summary>
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            // 建立新的電話簿條目
+            PhoneBookEntry entry;
+            entry.name = nameTextBox.Text.Trim();
+            entry.phone = phoneTextBox.Text.Trim();
+            // 檢查姓名與電話號碼是否為空
+            if (string.IsNullOrEmpty(entry.name) || string.IsNullOrEmpty(entry.phone))
+            {
+                MessageBox.Show("姓名或電話號碼不能為空，請重新輸入。");
+                return;
+            }
+            // 這裡可加入將 entry 加入 phoneList 及更新顯示的程式碼
+        }
+    }
+}
